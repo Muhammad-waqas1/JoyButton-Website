31f68fb58e6809d58ce0cf0cc982df85d8555b2d
Waqas <waqas.print.404@gmail.com>
2024-07-05 18:06:33 +0500

    migrations

--- joybutton/game/views.py
+++ joybutton/game/views.py
@@ -1,25 +1,25 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'game/index.html')
+    return render(request, 'index.html')
 
 def memory_matching(request):
-    return render(request, 'game/memory_matching.html')
+    return render(request, 'memory_matching.html')
 
 def tic_tac_toe(request):
-    return render(request, 'game/tic_tac_toe.html')
+    return render(request, 'tic_tac_toe.html')
 
 def whack_a_mole(request):
-    return render(request, 'game/whack_a_mole.html')
+    return render(request, 'whack_a_mole.html')
 
 def snake_game(request):
-    return render(request, 'game/snake_game.html')
+    return render(request, 'snake_game.html')
 
 def puzzle_2048(request):
-    return render(request, 'game/puzzle_2048.html')
+    return render(request, 'puzzle_2048.html')
 
 def rock_paper_scissors(request):
-    return render(request, 'game/rock_paper_scissors.html')
+    return render(request, 'rock_paper_scissors.html')
 
 def hangman(request):
-    return render(request, 'game/hangman.html')
+    return render(request, 'hangman.html')
